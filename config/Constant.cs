56b9099ae257bc0bf54a763a56dc17552a6fbb46
bnquys <52300153@student.tdtu.edu.vn>
2025-04-06 15:10:02 +0700

    add constant employee role class

--- config/Constant.cs
+++ config/Constant.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace timber_shop_manager
+namespace timber_shop_manager.config
 {
     internal class Constant
     {
---
+++ config/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timber_shop_manager.config
+{
+    internal static class Employee
+    {
+        static class Role {
+            public static readonly string ACCOUNTANT = "Acountant";
+            public static readonly string MANAGER = "Manager";
+            public static readonly string SALE_AGENT = "Sale Agent";
+            public static readonly string ADMINISTRATOR = "Administrator";
+        }
+    }
+}
