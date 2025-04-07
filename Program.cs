a07eadd1cd24820dab60568a814d4b631081b7df
bnquys <52300153@student.tdtu.edu.vn>
2025-04-07 10:08:58 +0700

    sth v2

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace timber_shop_manager
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new frmMain());
-            Application.Run(new frmLogin());
+            Application.Run(new frmMain(new objects.Account("","")));
+            //Application.Run(new frmLogin());
         }
     }
 }
