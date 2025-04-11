6063a79c4bcce56687c189195254e891621ccdf6
bnquys <52300153@student.tdtu.edu.vn>
2025-04-11 14:00:15 +0700

    change product form

---
+++ objects/Catagory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace timber_shop_manager.objects
+{
+    internal class Catagory
+    {
+        public static readonly string PREFIX = "P";
+        public static readonly int CODE_LENGTH = 6;
+
+        private static DatabaseHelper dbHelper = new DatabaseHelper();
+        public static string getName(string id)
+        {
+            if (id != null)
+            {
+                string query = "SELECT Name FROM Catagory WHERE CatagoryID = @id";
+                string categoryName = Convert.ToString(dbHelper.ExecuteScalar(query, new SqlParameter("@id", id)));
+                return (categoryName != null) ? categoryName : string.Empty;
+            }
+            return string.Empty;
+        }
+
+        public static string getId(string name)
+        {
+            string query = "SELECT CatagoryId FROM Catagory WHERE Name = @name";
+            return Convert.ToString(dbHelper.ExecuteScalar(query, new SqlParameter("@name", name)));
+        }
+    }
+}
--- objects/Product.cs
+++ objects/Product.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Data.SqlClient;
 
 namespace timber_shop_manager.objects
 {
     internal class Product
     {
         public static readonly int SOLD_OUT = 0;
-        public static readonly string PREFIX = "SI";
-        public static readonly int CODE_LENGTH = 6;
+        public static readonly string PREFIX = "P";
+        public static readonly int CODE_LENGTH = 5;
 
-        private string id, name, unit, description;
+        private static DatabaseHelper dbHelper = new DatabaseHelper();
+
+        private string id, categoryId, name, unit, description;
         private double price;
         private int warranty, quantity;
 
-        public Product(string id, string name, string unit, double price, int warranty, string description, int quantity)
+        public Product(string id, string categoryId, string name, string unit, double price, int warranty, string description, int quantity)
         {
             this.id = id;
+            this.categoryId = categoryId;
             this.name = name;
             this.unit = unit;
             this.price = price;
@@ -30,6 +37,56 @@ namespace timber_shop_manager.objects
         public override string? ToString()
         {
             return $"{id.ToString().Trim().ToLower()} - {name} - {quantity} {unit}";
+        }
+
+        public static void add(Product product) 
+        {
+            string queryInsert = "INSERT INTO Product (ProductID, CatagoryID, Name, CalculationUnit, PriceQuotation, CustomerWarranty, Description, Quantity) " +
+                        "VALUES (@ID, @CatagoryID, @Name, @CalUnit, @PriceQuo, @Warranty, @Description, @Quantity)";
+            string queryUpdate = "UPDATE Product " +
+                "SET CatagoryID = @CatagoryID, " +
+                    "Name = @Name, " +
+                    "CalculationUnit = @CalUnit, " +
+                    "PriceQuotation = @PriceQuo, " +
+                    "CustomerWarranty = @Warranty, " +
+                    "Description = @Description, " +
+                    "Quantity = @Quantity " +
+                "WHERE ProductID = @ID";
+
+            string query = "SELECT COUNT(*) FROM Product WHERE ProductId = @id";
+            bool isExists = Convert.ToInt32(dbHelper.ExecuteScalar(query, new SqlParameter("@id", product.id))) != 0;
+
+            if (!isExists) 
+            {
+                dbHelper.ExecuteNonQuery(queryInsert,
+                    new SqlParameter("@ID", product.id),
+                    new SqlParameter("@CatagoryID", product.categoryId),
+                    new SqlParameter("@Name", product.name),
+                    new SqlParameter("@CalUnit", product.unit),
+                    new SqlParameter("@PriceQuo", product.price),
+                    new SqlParameter("@Warranty", product.warranty),
+                    new SqlParameter("@Description", product.description),
+                    new SqlParameter("@Quantity", product.quantity));
+            } 
+            else
+            {
+                dbHelper.ExecuteNonQuery(queryUpdate,
+                    new SqlParameter("@ID", product.id),
+                    new SqlParameter("@CatagoryID", product.categoryId),
+                    new SqlParameter("@Name", product.name),
+                    new SqlParameter("@CalUnit", product.unit),
+                    new SqlParameter("@PriceQuo", product.price),
+                    new SqlParameter("@Warranty", product.warranty),
+                    new SqlParameter("@Description", product.description),
+                    new SqlParameter("@Quantity", product.quantity));
+            }
+        }
+
+        public static void delete(Product product)
+        {
+            string query = "DELETE FROM Product WHERE ProductId = @id";
+
+            dbHelper.ExecuteNonQuery(query, new SqlParameter("@id", product.id));
         }
     }
 }
