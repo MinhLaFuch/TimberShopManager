3c18a8a77eef65198cd216d150479d9edde3eabd
Minh La Fuch <ph18122005@gmail.com>
2025-04-17 12:34:59 +0700

    add OOP

---
+++ objects/ImportInvoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timber_shop_manager.objects
+{
+    internal class ImportInvoice
+    {
+    }
+}
