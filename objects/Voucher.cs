4a27d20634c019cef0b0ef95f0637b9346edad83
bnquys <52300153@student.tdtu.edu.vn>
2025-04-22 15:04:26 +0700

    add Voucher class

--- objects/Voucher.cs
+++ objects/Voucher.cs
@@ -3,11 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace timber_shop_manager.objects
 {
     public class Voucher
     {
+        private static DatabaseHelper dbHelper = new();
 
+        public static readonly string PREFIX = "V";
+        public static readonly int CODE_LENGTH = 4;
+
+        private string id, name, description;
+        private DateTime startDate, endDate;
+        private float percentant;
+        private int price;
+
+        public Voucher(string id, string name, DateTime startDate, DateTime endDate, string description, string percentant, string price)
+        {
+            this.id = id.Trim();
+            this.name = name.Trim();
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.description = description.Trim();
+            this.percentant = float.Parse(percentant.Trim());
+            this.price = int.Parse(price.Trim());
+        }
+
+        public static void add(Voucher v)
+        {
+            string query = "SELECT COUNT(Id) FROM Voucher WHERE Id = @id";
+            int count = Convert.ToInt32(dbHelper.ExecuteScalar(query, new SqlParameter("@id", v.id)));
+
+            if (count != 0)
+            {
+                MessageBox.Show("Voucher với ID " + v.id + " đã tồn tại. Không thể thêm mới.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                query = "INSERT INTO Voucher (Id, Name, StartDate, EndDate, Description, Percentant, Price) VALUES " +
+                        "(@id, @name, @startDate, @endDate, @description, @percentant, @price)";
+
+                dbHelper.ExecuteNonQuery(query,
+                    new SqlParameter("@id", v.id),
+                    new SqlParameter("@name", v.name),
+                    new SqlParameter("@startDate", v.startDate),
+                    new SqlParameter("@endDate", v.endDate),
+                    new SqlParameter("@description", v.description),
+                    new SqlParameter("@percentant", v.percentant),
+                    new SqlParameter("@price", v.price));
+
+                MessageBox.Show("Voucher với ID " + v.id + " đã được thêm thành công.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        public static Voucher getVoucherById(string voucherId)
+        {
+            string query = "SELECT * FROM Voucher WHERE Id = @id";
+            SqlDataReader reader = dbHelper.ExecuteReader(query, new SqlParameter("@id", voucherId));
+
+            if (reader.Read())
+            {
+                return new Voucher(
+                    reader["Id"].ToString(),
+                    reader["Name"].ToString(),
+                    Convert.ToDateTime(reader["StartDate"]),
+                    Convert.ToDateTime(reader["EndDate"]),
+                    reader["Description"].ToString(),
+                    reader["Percentant"].ToString(),
+                    reader["Price"].ToString()
+                );
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
